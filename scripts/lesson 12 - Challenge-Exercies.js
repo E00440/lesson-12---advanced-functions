f9c3141d3542ece3bc1c0ea503433c8ca27498ab
Emmanuel Malik <emmanuel00440@gmail.com>
2025-03-24 12:45:27 +0100

    final version

--- scripts/lesson 12 - Challenge-Exercies.js
+++ scripts/lesson 12 - Challenge-Exercies.js
@@ -1,4 +1,6 @@
 //12f
+// added arrow function in 12p and 12q
+/*
 let timeoutId;
 
 function addToCart() {
@@ -12,6 +14,7 @@ function addToCart() {
     addedText.innerHTML = '';
   }, 2000);
 }
+*/
 
 
 
@@ -51,7 +54,8 @@ function removeTitle() {
 
 //12i
 
-
+// added arrow function in 12p and 12q
+/*
 let messages = 2;
 
 let intervalId;
@@ -82,6 +86,152 @@ function stopNotification() {
   clearInterval(intervalId);
   document.title = 'App';
 }
+
+function addTitle() {
+  messages++;
+  displayNotification();
+}
+function removeTitle() {
+  if (messages > 0) {
+    messages--;
+
+    if (messages === 0) {
+      stopNotification();
+    }
+  }
+}
+*/
+
+
+//12p
+//added new features in 12q
+/*
+let timeoutId;
+
+function addToCart() {
+  const addedText = document.querySelector('.js-added-text');
+
+  addedText.innerHTML = 'Added';
+
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    addedText.innerHTML = '';
+  }, 2000);
+}
+
+
+
+let messages = 2;
+
+let intervalId;
+
+let isDisplayNotification;
+
+displayNotification();
+
+function displayNotification() {
+  if (isDisplayNotification) {
+    return;
+  }
+  isDisplayNotification = true;
+
+  intervalId = setInterval(() => {
+    if (document.title === 'App') {
+      document.title = `(${messages}) New messages`;
+    } else {
+      document.title = 'App';
+    }
+  }, 1000);
+
+}
+
+function stopNotification() {
+  isDisplayNotification = false;
+
+  clearInterval(intervalId);
+  document.title = 'App';
+}
+
+function addTitle() {
+  messages++;
+  displayNotification();
+}
+function removeTitle() {
+  if (messages > 0) {
+    messages--;
+
+    if (messages === 0) {
+      stopNotification();
+    }
+  }
+}
+*/
+
+
+
+//12q
+document.querySelector('.js-add-to-cart').addEventListener('click', () => {
+  addToCart();
+});
+
+
+let timeoutId;
+
+function addToCart() {
+  const addedText = document.querySelector('.js-added-text');
+
+  addedText.innerHTML = 'Added';
+
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    addedText.innerHTML = '';
+  }, 2000);
+}
+
+
+
+let messages = 2;
+
+let intervalId;
+
+let isDisplayNotification;
+
+document.querySelector('.js-add-button').addEventListener('click', () => {
+  addTitle();
+});
+
+
+document.querySelector('.js-remove-button').addEventListener('click', () => {
+  removeTitle();
+});
+
+displayNotification();
+
+function displayNotification() {
+  if (isDisplayNotification) {
+    return;
+  }
+  isDisplayNotification = true;
+
+  intervalId = setInterval(() => {
+    if (document.title === 'App') {
+      document.title = `(${messages}) New messages`;
+    } else {
+      document.title = 'App';
+    }
+  }, 1000);
+
+}
+
+function stopNotification() {
+  isDisplayNotification = false;
+
+  clearInterval(intervalId);
+  document.title = 'App';
+}
+
 
 function addTitle() {
   messages++;
--- scripts/lesson 12 - Exercises.js
+++ scripts/lesson 12 - Exercises.js
@@ -32,6 +32,8 @@ function updateButton() {
 
 
 //12d
+// added arrow function in 12p and 12q
+/*
 function updateButton() {
   const buttonElement = document.querySelector('.js-button');
 
@@ -41,9 +43,12 @@ function updateButton() {
     buttonElement.innerHTML = 'Finished';
   }, 1000);
 }
+*/
 
 
 //12e
+// added arrow function in 12p and 12q
+/*
 function addToCart() {
   const addedText = document.querySelector('.js-added-text');
 
@@ -53,3 +58,158 @@ function addToCart() {
     addedText.innerHTML = '';
   }, 2000);
 }
+*/
+
+
+
+//Part 2
+/*
+//12j
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+console.log(multiply(2, 3));
+console.log(multiply(7, 10));
+*/
+
+//12k
+const multiply = (num1, num2) => num1 * num2;
+
+console.log(multiply(2, 3));
+console.log(multiply(7, 10));
+
+
+//12l
+function countPositive(nums) {
+  let count = 0;
+
+  nums.forEach((value, index) => {
+    if (value > 0) {
+      count++;
+    }
+  });
+  return count;
+}
+
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
+
+
+//12m
+function addNum(array, num) {
+  return array.map(value => value + num);
+}
+console.log(addNum([1, 2, 3], 2));
+console.log(addNum([-2, -1, 0, 99], 2));
+
+
+//12n
+/*
+function removeEgg(foods) {
+  return foods.filter((value) => {
+    return value !== 'egg';
+  });
+}
+
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+*/
+
+
+//12o
+//remove first two eggs
+function removeEgg(foods) {
+  let count = 0;
+  
+  return foods.filter((value, index) => {
+    if (value === 'egg') {
+      count++;
+      return count > 2;
+    }
+    return true;
+  });
+}
+
+//supersimpledev's solution
+/*
+function removeEgg(foods) {
+  let eggsRemoved = 0;
+
+  return foods.filter((food) => {
+    // If the food is 'egg', we should return false
+    // but only if we haven't removed 2 eggs already.
+    if (food === 'egg' && eggsRemoved < 2) {
+      eggsRemoved++;
+      return false;
+    }
+
+    return true;
+  });
+}
+*/
+
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+
+
+
+//12p
+//added new features in 12q
+/*
+function updateButton() {
+  const buttonElement = document.querySelector('.js-button');
+
+  buttonElement.innerHTML = 'Loading...';
+
+  setTimeout(() => {
+    buttonElement.innerHTML = 'Finished';
+  }, 1000);
+}
+
+
+function addToCart() {
+  const addedText = document.querySelector('.js-added-text');
+
+  addedText.innerHTML = 'Added';
+
+  setTimeout(() => {
+    addedText.innerHTML = '';
+  }, 2000);
+}
+*/
+
+
+
+//12q
+const buttonElement = document.querySelector('.js-button');
+
+buttonElement.addEventListener('click', () => {
+  updateButton();
+});
+
+function updateButton() {
+  const buttonElement = document.querySelector('.js-button');
+
+  buttonElement.innerHTML = 'Loading...';
+
+  setTimeout(() => {
+    buttonElement.innerHTML = 'Finished';
+  }, 1000);
+}
+
+
+const addToCartElement = document.querySelector('.js-add-to-cart');
+
+addToCartElement.addEventListener('click', () => {
+  addToCart();
+});
+
+function addToCart() {
+  const addedText = document.querySelector('.js-added-text');
+
+  addedText.innerHTML = 'Added';
+
+  setTimeout(() => {
+    addedText.innerHTML = '';
+  }, 2000);
+}
+
